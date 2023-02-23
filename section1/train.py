cc66dc058ca91a22ea52546c5e0153c4f2b8ee2f
aaplakhin <plaha.1996@mail.ru>
2023-02-23 20:27:37 +0300

    huy

--- section1/train.py
+++ section1/train.py
@@ -7,12 +7,58 @@ from unet import Unet
 from dataset import get_train_data
 
 
+class СustomScaler:
+    def __init__(self, scale_factor: float = 2 ** 10, scaler_type: str = "dynamic"):
+        self.scale_factor = scale_factor
+        self.counter = 0
+        self.scaler_type = scaler_type
+
+    def scale(self, loss):
+        return loss * self.scale_factor
+
+    @staticmethod
+    def count_inf_grads(optimizer):
+        inf_grads = 0
+        for g in optimizer.param_groups:
+            for p in g['params']:
+                if p.grad is not None:
+                    inf_grads += (torch.logical_not(torch.isfinite(p.grad))).sum().item()
+
+        return inf_grads
+
+    def unscale_grads(self, optimizer):
+        for g in optimizer.param_groups:
+            for p in g['params']:
+                if p.grad is not None:
+                    p.grad /= self.scale_factor
+
+    def step(self, optimizer):
+
+        if self.scaler_type == "dynamic":
+            inf_grads = self.count_inf_grads(optimizer)
+            if inf_grads > 0:
+                self.scale_factor /= 2
+                self.counter = 0
+            else:
+                self.counter += 1
+                if self.counter > 10:
+                    self.scale_factor *= 2
+                    self.counter = 0
+
+                self.unscale_grads(optimizer)
+                optimizer.step()
+        else:
+            self.unscale_grads(optimizer)
+            optimizer.step()
+
+
 def train_epoch(
-    train_loader: torch.utils.data.DataLoader,
-    model: torch.nn.Module,
-    criterion: torch.nn._Loss,
-    optimizer: torch.optim.optimizer.Optimizer,
-    device: torch.device,
+        train_loader: torch.utils.data.DataLoader,
+        model: torch.nn.Module,
+        criterion,
+        optimizer: torch.optim.Optimizer,
+        device: torch.device,
+        scaler
 ) -> None:
     model.train()
 
@@ -24,21 +70,23 @@ def train_epoch(
         with torch.cuda.amp.autocast():
             outputs = model(images)
             loss = criterion(outputs, labels)
-        # TODO: your code for loss scaling here
+
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        optimizer.zero_grad()
+        print(scaler.scale_factor)
 
         accuracy = ((outputs > 0.5) == labels).float().mean()
 
         pbar.set_description(f"Loss: {round(loss.item(), 4)} " f"Accuracy: {round(accuracy.item() * 100, 4)}")
 
 
-def train():
+def train(scaler):
     device = torch.device("cuda:0")
     model = Unet().to(device)
     criterion = nn.BCEWithLogitsLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
-
     train_loader = get_train_data()
-
     num_epochs = 5
     for epoch in range(0, num_epochs):
-        train_epoch(train_loader, model, criterion, optimizer, device=device)
+        train_epoch(train_loader, model, criterion, optimizer, device=device, scaler=scaler)

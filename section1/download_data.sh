929e97be4e407a813af63ee57fa5b9c01b51e64d
aaplakhin <plaha.1996@mail.ru>
2023-02-24 00:49:19 +0300

    as

--- section1/download_data.sh
+++ section1/download_data.sh
@@ -1,3 +1,3 @@
-wget https://www.dropbox.com/s/emtblbcjnid4ynh/CARAVANA.zip
-unzip -q CARAVANA.zip
-rm -rf ./train/.DS_Store ./train_masks/.DS_Store CARAVANA.zip
+wget https://www.dropbox.com/s/z54mil007y2hg00/CARVANA.zip
+unzip -q CARVANA.zip
+rm -rf ./train/.DS_Store ./train_masks/.DS_Store CARVANA.zip
